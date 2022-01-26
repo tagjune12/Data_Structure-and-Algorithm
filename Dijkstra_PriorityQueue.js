3cefb5b0d6c6013b908d0d99cbb1a0c55ca7676d
tagjune12 <tagjune12@gmail.com>
2022-01-26 16:51:32 +0900

    우선순위큐 다익스트라 추가

---
+++ Dijkstra_PriorityQueue.js
@@ -0,0 +1,39 @@
+function compare(a, b) {
+  return a['cost'] - b['cost'];
+}
+
+const graph = [
+  [],
+  [{ node: 2, cost: 2 }, { node: 3, cost: 5 }, { node: 4, cost: 1 }],
+  [{ node: 1, cost: 2 }, { node: 3, cost: 3 }, { node: 4, cost: 2 }],
+  [{ node: 1, cost: 5 }, { node: 2, cost: 3 }, { node: 4, cost: 3 }, { node: 5, cost: 1 }, { node: 6, cost: 5 }],
+  [{ node: 1, cost: 1 }, { node: 2, cost: 2 }, { node: 3, cost: 3 }, { node: 5, cost: 1 }],
+  [{ node: 3, cost: 1 }, { node: 4, cost: 1 }, { node: 6, cost: 2 }],
+  [{ node: 3, cost: 5 }, { node: 5, cost: 2 }]
+];
+
+function dijkstra(start) {
+  let distance = Array(graph.length).fill(Number.POSITIVE_INFINITY);
+  let queue = [];
+
+  distance[start] = 0;
+  queue.push({ node: start, cost: 0 });
+  queue.sort(compare);
+
+  while (queue.length > 0) {
+    const { node: curNode, cost } = queue.shift();
+
+    graph[curNode].forEach((value) => {
+      const { node: nextNode, cost: nextCost } = value;
+      if (distance[curNode] + nextCost < distance[nextNode]) {
+        distance[nextNode] = distance[curNode] + nextCost;
+        queue.push({ node: nextNode, cost: distance[nextNode] });
+      }
+    });
+    queue.sort(compare);
+  }
+
+  return distance;
+};
+
+console.log("최단경로:", dijkstra(1));
